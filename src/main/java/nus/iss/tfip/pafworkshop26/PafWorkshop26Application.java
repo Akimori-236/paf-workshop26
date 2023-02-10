187e79f52e34b0525d53535ed722bfa455e73362
Wee Seng <wee.bscl@gmail.com>
2023-02-10 15:27:54 +0800

    init setup

---
+++ src/main/java/nus/iss/tfip/pafworkshop26/PafWorkshop26Application.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafworkshop26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafWorkshop26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafWorkshop26Application.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/tfip/pafworkshop26/PafWorkshop26ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafworkshop26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafWorkshop26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
